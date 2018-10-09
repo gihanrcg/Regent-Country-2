24fd5a79b4288aa568fc81b29c411d3b6633b1d3
Gihan Siriwardhana <gihan.siriwardhana@ifsworld.com>
2018-10-09 09:37:39 +0530

    update bookings ui added

---
+++ app/src/main/java/com/example/gisilk/onlineorder2/com/functions/Booking/MyBookings.java
@@ -0,0 +1,15 @@
+package com.example.gisilk.onlineorder2.com.functions.Booking;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.gisilk.onlineorder2.R;
+
+public class MyBookings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_bookings);
+    }
+}
